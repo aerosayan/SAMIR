6970d761dc0d7c42ca3f87145047df351359bb4c
__SYN__ <aero.sayan@gmail.com>
2017-11-24 19:53:18 +0530

    Moved to new system

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,7 @@
 
 import samirStandardV1 as sv
 
+#------------------------------------------------------------------------------
 def findNextLex(xi,toFind,lexd):
 	n = lexd.__len__()
 	i = xi
@@ -17,6 +18,7 @@ def findNextLex(xi,toFind,lexd):
 		print('LEX NOT FOUND')
 		return -1	
 
+#------------------------------------------------------------------------------
 def collectSplineObjects(xi,forLex,lexd):
 	#cfg := [collectLex,sv.LSQR,sv.COLLECT_INDICES,sv.RSQR]
 	start = findNextLex(xi,sv.LSQR,lexd)
@@ -39,6 +41,7 @@ def collectSplineObjects(xi,forLex,lexd):
 	print('splineObjects have been prepared')
 	return splineObjects
 
+#------------------------------------------------------------------------------
 def makeLexCollectionOrder(xi,collect,lexd):
 	#  Produce the order in which the lex are to be collected
 	# eg { Line[1,2] + Spline[3,4] + Line[3,4] => return [LINE,SPLINE,LINE]
@@ -75,6 +78,7 @@ def makeLexCollectionOrder(xi,collect,lexd):
 		exit()
 	return collectOrder,collectOrderPos
 
+#------------------------------------------------------------------------------
 def collectPointsFromSplice(xi,collect,lexd):
 	n = lexd.__len__()
 	i = xi # lexd iterator index
@@ -85,50 +89,42 @@ def collectPointsFromSplice(xi,collect,lexd):
 	m = collectOrder.__len__()
 	# matrix or 2d list used to store all the indices of collectOrder
 	collectionMatrix = [[]]
-	# collection matrix cound : as lex indices are collected the row no. will increase
+	# collection matrix count : collect lex indices and store it in rows
 	cmCount = 0;
-	while i<n: #{ Iterate through lexd
-		I = i
-		i = i+1 # update lexd iterator
-		lex = lexd[I]
-		# Go through the lexd from xi to n
-		# find number of occurences  and positions of the lex in collect
-		# and arrange them in an ascending order eg [LINE,SPLINE,LINE] =>
-		# check if all the lex in lexd in the collect region are collectable or not
-		# run the index collector for each of the elements and form two arrays
-		# to contain x and y co-ordinates of the points ??????
-		while j<m: #{ Iterate through collectOrder list
-			J = j
-			j = j+1 # update collect iterator
-			c = collectOrder[J]
-			if(c.uid == sv.POINT.uid):
-				print('Collecting POINT')
-				print('exit mark4')
-				exit()
-			elif(c.uid == sv.LINE.uid):
-				print('Collecting LINE')
-				collectionMatrix[cmCount] = collectSplineObjects(collectOrderPos[J],sv.LINE,lexd)
-				collectionMatrix.append([]) # create space for next collection
-				cmCount += 1 # Increase collection matrix counter
-			elif(c.uid == sv.SPLINE.uid):
-				print('Collecting SPLINE')
-				collectionMatrix[cmCount] = collectSplineObjects(collectOrderPos[J],sv.SPLINE,lexd)
-				collectionMatrix.append([]) # create space for next collection
-				cmCount += 1 # Increase collection matrix counter
-				print('mark 10')
-
-				exit()
-			else:
-				print('Could not collect the requseted lexeme')
-				print(collect)
-				exit()
-		#}
-	print('Finished iterations')
-	return 1
-
+	# Find number of occurences  and positions of the lex in collect
+	# and arrange them in an ascending order eg [LINE,SPLINE,LINE] =>
+	# check if all the lex in lexd in the collect region are collectable or not
+	# run the index collector for each of the elements and form two arrays
+	# to contain x and y co-ordinates of the points ??????
+	while j<m: #{ Iterate through collectOrder list
+		J = j
+		j = j+1 # update collect iterator
+		c = collectOrder[J]
+		if(c.uid == sv.POINT.uid):
+			print('INF :: Collecting POINT')
+			print('exit mark4')
+			exit()
+		elif(c.uid == sv.LINE.uid):
+			print('INF :: Collecting LINE')
+			collectionMatrix[cmCount] = collectSplineObjects(collectOrderPos[J],
+			                                                 sv.LINE,lexd)
+			collectionMatrix.append([]) # create space for next collection
+			cmCount += 1 # Increase collection matrix counter
+		elif(c.uid == sv.SPLINE.uid):
+			print('INF :: Collecting SPLINE')
+			collectionMatrix[cmCount] = collectSplineObjects(collectOrderPos[J],
+			                                                 sv.SPLINE,lexd)
+			collectionMatrix.append([]) # create space for next collection
+			cmCount += 1 # Increase collection matrix counter
+		else:
+			print('ERR :: Could not collect the requseted lexeme')
+			print(collect)
+			exit()
 	#}
+	print('INF :: Finished collecting splice objects from splice')
+	return collectionMatrix,collectOrder,collectOrderPos
 
-
+#------------------------------------------------------------------------------
 def verifyCFG(xi,cfg,collect,lexd):
 	n = lexd.__len__()
 	i = xi # lexd iterator index
@@ -143,13 +139,16 @@ def verifyCFG(xi,cfg,collect,lexd):
 		if(c.uid == sv.COLLECT.uid):
 			# Within the iteration 
 			# Do collection
-			collectPointsFromSplice(I,collect,lexd)
+			"""
+			@params
+			cmat : Collection matrix
+			corder : Collection oreder
+			corderpos : Collection order position
+			"""
+			cmat,corder,corderdpos = collectPointsFromSplice(I,collect,lexd)
+
 			# skip collected entitites
 			print('exit mark2')
-			exit()
-		elif(c.uid == sv.COLLECT_INDICES.uid):
-			print('Collecting indices')
-			print('exit mark6')
 			exit()
 		elif(c.uid == lexd[I].uid):
 			# cfg grammar verified
@@ -187,6 +186,7 @@ def parserStdV1(lexdata):
 
 			collect = [sv.LINE,sv.SPLINE]
 			#print(cfg)
+
 			verifyCFG(I,cfg,collect,lexd)
 			print('EXIT mark1')	
 			exit()
